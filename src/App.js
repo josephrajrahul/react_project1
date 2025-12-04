3b3cd41e3caabe3afbc0eb5e85d9b35a39cb9cd9
josephrajrahul <josephrajrahul@gmail.com>
2025-12-04 14:08:53 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
