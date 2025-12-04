241345e14be55d9dad6a6796c1b07f116a1001bd
josephrajrahul <josephrajrahul@gmail.com>
2025-12-04 14:09:59 +0530

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This website is deployed using Azure Static Web Apps.</p>
+    </div>
+  );
+}
+
+export default About;
