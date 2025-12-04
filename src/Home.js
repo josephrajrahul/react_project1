8595febc82528b0a948d66eb0a6d6918d3e7edca
josephrajrahul <josephrajrahul@gmail.com>
2025-12-04 14:09:31 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My React Website</h1>
+      <p>This is a simple homepage built with React.</p>
+    </div>
+  );
+}
+
+export default Home;
